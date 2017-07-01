3d72ce1eccb2355ae4388f7036211f913198c6ce
Administrator <abhi_sri_amity6@yahoo.com>
2017-07-01 17:16:16 +0530

    completed

--- VirtualMethodAndOverride/Program.cs
+++ VirtualMethodAndOverride/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace VirtualMethodAndOverride
 {
+    class BaseClass
+    {
+        string currentclass1;
+       public virtual void who()
+       {
+            Console.WriteLine(" Who() in base class");
+       }
+    }
+    class DerivedClass1 : BaseClass
+    {
+        public override void who()
+        {
+            Console.WriteLine(" Who() in DerivedClass1 class");
+        }
+    }
+    class DerivedClass2 : BaseClass
+    {
+        public override void who()
+        {
+            Console.WriteLine(" Who() in DerivedClass2 class");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            BaseClass bobj1 = new BaseClass();
+            BaseClass ObjRef;
+            DerivedClass1 Dobj1 = new DerivedClass1();
+            DerivedClass2 Dobj2 = new DerivedClass2();
+
+            ObjRef = bobj1;
+            ObjRef.who();
+            ObjRef = Dobj1;
+            ObjRef.who();
+            ObjRef = Dobj2;
+            ObjRef.who();
+
+            Console.Read();
         }
     }
 }
